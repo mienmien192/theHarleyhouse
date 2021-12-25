a5a19e476cf079bba9444ac1710b48268f5cf2cd
Hạ Trần Miên Khánh <mienmien192@gmail.com>
2021-12-25 09:47:57 +0700

    sua lan cuoi

--- routes/function.js
+++ routes/function.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const categoryModel = require('../models/category.model')
 const productModel = require('../models/product.model')
-
+const nodemailer = require('nodemailer');
 router.get('/allProduct', async(req, res) => {
     try {
         const products = await productModel.find().populate('category', ['name'])
@@ -27,10 +27,38 @@ router.get('/aboutUs', (req, res) => {
     res.render('functions/aboutUs')
 })
 
-router.get('/specialOffer', (req, res) => {
-    res.render('functions/specialOffer')
+router.get('/contactUs', (req, res) => {
+    res.render('functions/contactUs')
 })
+router.post('/contactUs', (res, req) => {
+    console.log(req.body)
 
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hatran192.org@gmail.com',
+            pass: 'Khanhha192'
+
+        }
+    });
+
+    const mainOptions = {
+        from: 'hatran192.org@gmail.com',
+        to: 'phamd090420@gmail.com',
+        subject: `Message from TheHarleyC&T`,
+        text: 'That was easy!'
+    }
+
+    transporter.sendMail(mainOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+
+        } else {
+
+
+        }
+    })
+})
 router.get('/recruitment', (req, res) => {
     res.render('functions/recruitment')
 })
